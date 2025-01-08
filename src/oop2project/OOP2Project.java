43078f2ea0c5b216dcf86137330011fb748a6bd0
xRana <127047803+xRana@users.noreply.github.com>
2025-01-08 19:59:41 +0300

    Add files via upload

---
+++ src/oop2project/OOP2Project.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop2project;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author rana-
+ */
+public class OOP2Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FirstPage frm = new FirstPage();
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frm.setVisible(true);
+  
+        
+    }
+    
+}
